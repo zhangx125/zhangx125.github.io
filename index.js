dbc80998fcbbc30b5d744866bcce4932bb516a50
Xin Zhang <zhangx1252@gmail.com>
2025-08-13 16:59:11 -0700

    Change some details of the map

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let map;
 
 async function initMap() {
   // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
+  const position = { lat: 34.1517907, lng: -118.7572127 };
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
@@ -11,7 +11,7 @@ async function initMap() {
 
   // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 10,
     center: position,
     mapId: "DEMO_MAP_ID",
   });
@@ -20,7 +20,7 @@ async function initMap() {
   const marker = new AdvancedMarkerElement({
     map: map,
     position: position,
-    title: "Uluru",
+    title: "Coin Mine",
   });
 }
 
